a512c908228ae78f0456556bd1e99df5bcca8471
Ali ElFarou <aelfa057@uottawa.ca>
2017-02-27 19:05:50 +0000

    Add session functionality, and restrictions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController 
    
+   before_action :set_user, only: [:edit, :update, :show]
+   before_action :require_same_user, only: [:edit, :update]
+   
    def new
      @user = User.new
    end
@@ -23,14 +26,14 @@ class UsersController < ApplicationController
    end
    
    def edit
-       @user = User.find(params[:id])
+       
    end
    
    def update
        @user = User.find(params[:id])
        if @user.update(user_params)
           #flash[:success] = "Account successfully updated"
-          redirect_to users_path
+          redirect_to user_path
        else
           render 'edit'
        end
@@ -44,5 +47,15 @@ class UsersController < ApplicationController
    def user_params
       params.require(:user).permit(:username, :email, :password)
    end
+   
+   def set_user
+      @user= User.find(params[:id])
+   end
     
+   def require_same_user
+      if current_user != @user
+      #flash[:danger] = "You can only edit your own account"
+      redirect_to root_path
+      end
+   end
 end
