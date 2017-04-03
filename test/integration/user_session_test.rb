cb999fe03aecbc7bc837c264f34a41987fc22e5a
Ali ElFarou <aelfa057@uottawa.ca>
2017-04-03 14:48:24 +0000

    Add integration tests

---
+++ test/integration/user_session_test.rb
@@ -0,0 +1,53 @@
+require 'test_helper'
+
+class UserSessionTest < ActionDispatch::IntegrationTest
+   
+  def setup
+    @user = User.create(username: "john", email: "john@example.com", password: "password")
+    @team = Team.create(name: "testTeam")
+  end
+    
+    test "Sign in and show user page" do
+       sign_in_as(@user, "password")
+       get user_path(@user.id)
+       assert_template 'users/show'
+    end
+    
+    test "Sign in and open create team link" do
+       sign_in_as(@user, "password")
+       get new_team_path
+        assert_template 'teams/new'
+    end
+    
+    test "sign in and show user teams page" do
+      sign_in_as(@user, "password")
+      get teams_path
+      assert_template 'teams/index'
+    end
+    
+  test "Create team" do
+    sign_in_as(@user, "password")
+    get new_team_path
+    post_via_redirect teams_path, team: {name: "test"}
+    #get teams_path()
+    assert_template 'welcome/home'
+  end
+  
+  test "Automatic Login after signup" do
+    get signup_path
+    post_via_redirect users_path, user: {username: "testuser", email: "testuser@example.com", password: "password"}
+    assert_template 'users/show'
+  end
+  
+  test "sign up, create team, and view list of teams" do
+    get signup_path
+    post_via_redirect users_path, user: {username: "testuser", email: "testuser@example.com", password: "password"}
+    assert_template 'users/show'
+    get new_team_path
+    post_via_redirect teams_path, team: {name: "test2"}
+    assert_template 'welcome/home'
+    get teams_path
+    assert_template 'teams/index'
+  end
+  
+end
