3b808735f36ac8b49442ab029caccccf18d334a9
Ramy ElFarou <relfa048@uottawa.ca>
2017-05-10 20:55:09 +0000

    Fixed winner_id foreign key inside matches table

--- db/migrate/20170415202555_add_winner_to_match.rb
+++ db/migrate/20170415202555_add_winner_to_match.rb
@@ -1,6 +1,5 @@
 class AddWinnerToMatch < ActiveRecord::Migration
   def change
     add_reference :matches, :winner, references: :teams, index: true
-    add_foreign_key :matches, :teams, column: :winner
   end
 end
